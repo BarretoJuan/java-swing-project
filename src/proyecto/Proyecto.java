b7f20eca83a48a38872b9a9d8370978db68d4440
Juan Barreto <juan2.manuelbarreto@gmail.com>
2024-01-23 15:05:17 -0400

    Colors and icon

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -1,15 +1,24 @@
 package proyecto;
 import java.awt.*;
+import java.net.URL;
 import javax.swing.*;
+import proyecto.Colores;
 
 public class Proyecto extends JFrame{
     public Proyecto() {
+
+      URL iconURL = getClass().getResource("/resources/notepad.png");
+      Image icon = new javax.swing.ImageIcon(iconURL).getImage();
+      this.setIconImage(icon);
       this.setSize(640,512); //Tamanio de la ventana
-      this.setTitle("Proyecto"); // Titulo de la ventana
+      this.setTitle("Datos Personales"); // Titulo de la ventana
       this.setLocationRelativeTo(null); // Ubicar la ventana en el centro de la pantalla
       this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Terminar la ejecucion del programa cuando se cierre la ventana 
       this.getContentPane().setBackground(Color.BLACK); // Se establece un color de fondo para la ventana
+      this.setResizable(false);
       iniciarComponentes();
+
+
     }
     
     
@@ -17,16 +26,11 @@ public class Proyecto extends JFrame{
             JPanel panel = new JPanel();
             panel.setLayout(null);     
             this.getContentPane().add(panel);
-            Color azul = new Color(52,232,232,255);
-
-
-            
-  
             JLabel etiqueta = new JLabel();
             etiqueta.setText("Hola Mundo");
-            etiqueta.setBounds(25, 30, 120, 20);
+            etiqueta.setBounds(0, 30, 640, 20);
             etiqueta.setForeground(Color.white);
-            etiqueta.setBackground(azul);
+            etiqueta.setBackground(Colores.darkBlue);
             etiqueta.setOpaque(true);
             panel.add(etiqueta);
             
