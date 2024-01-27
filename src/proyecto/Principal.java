3a72755b1d3fdfce5a8a0220102b7048ca9bf27e
Juan Barreto <juan2.manuelbarreto@gmail.com>
2024-01-27 01:15:50 -0400

    Added Input Verifier

--- src/proyecto/Principal.java
+++ src/proyecto/Principal.java
@@ -1,9 +1,20 @@
 package proyecto;
 
+import java.util.Locale;
+
 public class Principal {
     
     public static void main(String[] args) {
         Proyecto p1 = new Proyecto();
         p1.setVisible(true);
+        String[] paises = Locale.getISOCountries();
+        int tamanio = paises.length;
+
+        int i;
+        for (i=0; i<tamanio; i++) {
+            System.out.println(paises[i]);
+        }
+
+        
     } //main()
 } //Principal class
--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -51,6 +51,7 @@ public class Proyecto extends JFrame{
             hint.setHint("Ingrese su código",entryCodigo);
             entryCodigo.setBounds(0,80,640,20);
             entryCodigo.setBackground(Colores.lightBlue);
+            entryCodigo.setInputVerifier(new Verify.CodigoVerifier());
             panel.add(entryCodigo);
             
             JLabel etiquetaCedula = new JLabel();
@@ -65,6 +66,7 @@ public class Proyecto extends JFrame{
             hint.setHint("Ingrese su cédula", entryCedula);
             entryCedula.setBounds(0,130,640,20);
             entryCedula.setBackground(Colores.lightBlue);
+            entryCedula.setInputVerifier(new Verify.CedulaVerifier());
             panel.add(entryCedula);
            
             JLabel etiquetaNombre = new JLabel();
@@ -79,6 +81,7 @@ public class Proyecto extends JFrame{
             hint.setHint("Ingrese sus nombres y apellidos", entryNombre);
             entryNombre.setBounds(0,180,640,20);
             entryNombre.setBackground(Colores.lightBlue);
+            entryNombre.setInputVerifier(new Verify.MaxLengthVerifier(25));
             panel.add(entryNombre);
 
             JLabel etiquetaFechaNacimiento = new JLabel();
@@ -89,11 +92,14 @@ public class Proyecto extends JFrame{
             etiquetaFechaNacimiento.setOpaque(true);
             panel.add(etiquetaFechaNacimiento);
         
-            JTextField entryFecha = new JTextField();
-            hint.setHint("Ingrese su fecha de nacimiento", entryFecha);
-            entryFecha.setBounds(0,230,640,20);
-            entryFecha.setBackground(Colores.lightBlue);
-            panel.add(entryFecha);
+            UIManager.put(CalendarHeaderHandler.uiControllerID, SpinningCalendarHeaderHandler.class.getName()); 
+            JXDatePicker picker = new JXDatePicker(new Locale("es")); //Initialize a JXDatePicker using the ISO language "es" for Spanish 
+            picker.getMonthView().setZoomable(true); //This line of code, and the UIManager one enable an experimental feature to allow yearly navigation through the calendar
+            picker.setFormats(new SimpleDateFormat("dd/MM/yyyy")); //Setting a date format
+            picker.getEditor().setEnabled(false); //The JXDatePicker Component is essentially made of two embedded components: The calendar itself, and a FormattedTextField where the input is displayed, the .getEditor() method allows us to interact specifically with the TextField, on this case I disabled it, in order to force the user to enter a date only using the Date Picker
+            picker.getEditor().setDisabledTextColor(Color.BLACK);  //After using the setEnabled(false) method, the FormattedTextField is visually disabled too, rendering the text in a transparent, blue-ish tone, this method overrides that color, making it look as if it wasn't disabled
+            picker.setBounds(0,230,600,20);
+            panel.add(picker);  
 
             JLabel etiquetaLugarNacimiento = new JLabel();
             etiquetaLugarNacimiento.setText(" Lugar de Nacimiento:");
@@ -107,6 +113,7 @@ public class Proyecto extends JFrame{
             hint.setHint("Ingrese su lugar de nacimiento", entryLugarNacimiento);
             entryLugarNacimiento.setBounds(0,280,640,20);
             entryLugarNacimiento.setBackground(Colores.lightBlue);
+            entryLugarNacimiento.setInputVerifier(new Verify.MaxLengthVerifier(50));
             panel.add(entryLugarNacimiento);
 
             JLabel etiquetaDireccion = new JLabel();
@@ -121,6 +128,7 @@ public class Proyecto extends JFrame{
             hint.setHint("Ingrese su dirección", entryDireccion);
             entryDireccion.setBounds(0,330,640,20);
             entryDireccion.setBackground(Colores.lightBlue);
+            entryDireccion.setInputVerifier(new Verify.MaxLengthVerifier(100));
             panel.add(entryDireccion);
 
             JLabel etiquetaNumTelf = new JLabel();
@@ -135,6 +143,7 @@ public class Proyecto extends JFrame{
             hint.setHint("Ingrese su número de teléfono", entryNumTelf);
             entryNumTelf.setBounds(0,380,640,20);
             entryNumTelf.setBackground(Colores.lightBlue);
+            entryNumTelf.setInputVerifier(new Verify.TelefonoVerifier());
             panel.add(entryNumTelf);
 
             JLabel etiquetaEmail = new JLabel();
@@ -149,23 +158,19 @@ public class Proyecto extends JFrame{
             hint.setHint("Ingrese su email", entryEmail);
             entryEmail.setBounds(0,430,640,20);
             entryEmail.setBackground(Colores.lightBlue);
+            entryEmail.setInputVerifier(new Verify.EmailVerifier());
             panel.add(entryEmail);      
             panel.setBackground(Colores.lightBlue);
             
 
-            UIManager.put(CalendarHeaderHandler.uiControllerID, SpinningCalendarHeaderHandler.class.getName()); 
-            JXDatePicker picker = new JXDatePicker(new Locale("es")); //Initialize a JXDatePicker using the ISO language "es" for Spanish 
-            picker.getMonthView().setZoomable(true); //This line of code, and the UIManager one enable an experimental feature to allow yearly navigation through the calendar
-            picker.setFormats(new SimpleDateFormat("dd/MM/yyyy")); //Setting a date format
-            picker.getEditor().setEnabled(false); //The JXDatePicker Component is essentially made of two embedded components: The calendar itself, and a FormattedTextField where the input is displayed, the .getEditor() method allows us to interact specifically with the TextField, on this case I disabled it, in order to force the user to enter a date only using the Date Picker
-            picker.getEditor().setDisabledTextColor(Color.BLACK);  //After using the setEnabled(false) method, the FormattedTextField is visually disabled too, rendering the text in a transparent, blue-ish tone, this method overrides that color, making it look as if it wasn't disabled
-            picker.setBounds(0,460,130,30);
 
 
 
-            panel.add(picker);
-  
+    
 
                   } // IniciarComponentes()\
         
+
+
+
 } // Proyecto Class
